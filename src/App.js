818877da5abac46e5bc408bec8f1ad090cf51135
gerritt <gmocore@gmail.com>
2020-05-01 16:44:58 -0600

    added form inputs to movie form

--- src/App.js
+++ src/App.js
@@ -16,11 +16,51 @@ const App = () => {
       })
       .catch((err) => console.log(err));
   }, []);
+
+  async function addMovie(title, genre, image) {
+    const imageFormData = new FormData()
+    imageFormData.append('file', image)
+    imageFormData.append('upload_preset', 'movie-list')
+    try {
+      const cloudinary = await axios(
+        {
+          method: "post",
+          url: 'https://api.cloudinary.com/v1_1/CLOUDINARY-USER-NAME/image/upload',
+          data: imageFormData,
+          headers: { 'Content-Type': 'multipart/form-data' }
+        }
+      )
+      console.log(cloudinary)
+      const flask_db = await axios(
+        {
+          method: "post",
+          url: 'http://localhost:5000/api/v1/movie',
+          data: {
+              title: title,
+              genre: genre,
+              // Either http or https
+              // image_url: cloudinary.data.url,
+              image_url: cloudinary.data.secure_url,
+              public_id: cloudinary.data.public_id
+          },
+          headers: {
+              'Content-Type': 'application/json'
+          }
+        }
+      )
+      console.log(flask_db)
+      setMovies([...movies, flask_db.data])
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
   return (
     <div className="app">
       <NavBar movies={movies} />
       <MovieList movies={movies} />
-      <MovieForm addMovie={""} />
+      <MovieForm addMovie={addMovie} />
     </div>
   );
 };
--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -3,11 +3,13 @@ import React, { useState } from "react";
 const MovieForm = ({ addMovie }) => {
   const [title, setTitle] = useState("");
   const [genre, setGenre] = useState("");
+  const [image, setImage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setTitle("");
     setGenre("");
+    setImage("");
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -23,6 +25,13 @@ const MovieForm = ({ addMovie }) => {
         value={genre}
         onChange={(e) => setGenre(e.target.value)}
       />
+      <div className="file-submit-wrapper">
+        <input
+          type="file"
+          onChange={e => setImage(e.target.files[0])}
+        />
+        <input type="submit" value="Add Movie" />
+      </div>
     </form>
   );
 };
